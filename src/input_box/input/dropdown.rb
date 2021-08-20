a277d43ba88b0b9c491bf0e9976dab67386d95cd
Thomas Thomassen <thomthom@sketchup.com>
2021-08-20 10:43:11 +0200

    Catch invalid default value in Dropdown and Listbox.

--- src/input_box/input/dropdown.rb
+++ src/input_box/input/dropdown.rb
@@ -7,6 +7,9 @@ module Example::HtmlInputBox
     class Dropdown < Input
 
       def initialize(label, default = nil, options = [])
+        if !default.nil? && !options.include?(default)
+          raise ArgumentError, 'default value is not a valid option'
+        end
         super(label: label, default: default, options: options)
       end
 
